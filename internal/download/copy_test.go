78600c0d5bbb1cd696fcd4a02ec68aac486c1a9e
Eric Chiang <eric.chiang.m@gmail.com>
2017-06-04 14:12:37 -0400

    internal/download: tests for copyFile

--- internal/download/copy_test.go
+++ internal/download/copy_test.go
@@ -2,7 +2,11 @@ package download
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
+	"reflect"
+	"sort"
 	"testing"
 	"time"
 )
@@ -29,38 +33,14 @@ func TestIgnore(t *testing.T) {
 		mode os.FileMode
 		want bool
 	}{
-		{
-			name: "hello.go",
-		},
-		{
-			// Ignore test files.
-			name: "hello_test.go",
-			want: true,
-		},
-		{
-			name: "dir",
-			mode: os.ModeDir,
-		},
-		{
-			// Don't want to ignore c files.
-			name: "foo.c",
-		},
-		{
-			// Don't want to ignore assembly files.
-			name: "foo.s",
-		},
-		{
-			name: "README.md",
-			want: true,
-		},
-		{
-			name: "symlink.go",
-			mode: os.ModeSymlink,
-			want: true,
-		},
-		{
-			name: "LICENSE",
-		},
+		{name: "hello.go"},
+		{name: "hello_test.go", want: true},
+		{name: "dir", mode: os.ModeDir},
+		{name: "foo.c"},
+		{name: "foo.s"},
+		{name: "README.md", want: true},
+		{name: "symlink.go", mode: os.ModeSymlink, want: true},
+		{name: "LICENSE"},
 	}
 	for _, test := range tests {
 		info := &fakeFile{test.name, test.mode}
@@ -69,4 +49,99 @@ func TestIgnore(t *testing.T) {
 			t.Errorf("ignore(%s), want=%t, got=%t", info, test.want, got)
 		}
 	}
+}
+
+func TestCopyFile(t *testing.T) {
+	p := "foo/bar.go"
+	test := copyTest{
+		files: []fakefile{
+			{"foo/bar.go", ""},
+			{"foo/foo.go", ""},
+		},
+		want: []string{"foo/bar.go"},
+		copyFiles: func(t *testing.T, dest, src string) {
+			destFile := filepath.Join(dest, p)
+			srcFile := filepath.Join(src, p)
+
+			info, err := os.Stat(srcFile)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if err := copyFile(destFile, srcFile, info); err != nil {
+				t.Fatal(err)
+			}
+		},
+	}
+	test.run(t)
+}
+
+type copyTest struct {
+	files []fakefile
+	want  []string
+
+	copyFiles func(t *testing.T, dest, src string)
+}
+
+func (c copyTest) run(t *testing.T) {
+	src, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(src)
+
+	dest, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dest)
+
+	for _, file := range c.files {
+		if err := file.write(src); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	c.copyFiles(t, dest, src)
+
+	got, err := listFilepaths(dest)
+	if err != nil {
+		t.Fatal(err)
+	}
+	sort.Strings(got)
+	sort.Strings(c.want)
+
+	if !reflect.DeepEqual(got, c.want) {
+		t.Errorf("expected files %q got %q", c.want, got)
+	}
+}
+
+type fakefile struct {
+	path     string
+	contents string
+}
+
+func (f fakefile) write(dir string) error {
+	p := filepath.Join(dir, f.path)
+	if err := os.MkdirAll(filepath.Dir(p), 0755); err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(p, []byte(f.contents), 0644)
+}
+
+func listFilepaths(dir string) ([]string, error) {
+	var files []string
+	f := func(path string, info os.FileInfo, err error) error {
+		if err != nil || info.IsDir() {
+			return err
+		}
+		rel, err := filepath.Rel(dir, path)
+		if err != nil {
+			return err
+		}
+		files = append(files, rel)
+		return nil
+	}
+
+	return files, filepath.Walk(dir, f)
 }
