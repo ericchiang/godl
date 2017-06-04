a79390bd828016fb05a73a2f5a17fece0cdc4dc3
Eric Chiang <eric.chiang.m@gmail.com>
2017-06-04 14:53:01 -0400

    internal/download: more tests

--- internal/download/copy_test.go
+++ internal/download/copy_test.go
@@ -54,7 +54,7 @@ func TestIgnore(t *testing.T) {
 func TestCopyFile(t *testing.T) {
 	p := "foo/bar.go"
 	test := copyTest{
-		files: []fakefile{
+		files: []testfile{
 			{"foo/bar.go", ""},
 			{"foo/foo.go", ""},
 		},
@@ -75,8 +75,94 @@ func TestCopyFile(t *testing.T) {
 	test.run(t)
 }
 
+func TestCopyDir(t *testing.T) {
+	test := copyTest{
+		files: []testfile{
+			{"foo/bar.go", ""},
+			{"foo/foo.go", ""},
+			{"foo/foo_test.go", ""},   // Ignore test files
+			{"foo/foo2/hello.go", ""}, // Ignore nested directories
+			{"bar/hello.go", ""},      // Not in directory
+		},
+		want: []string{
+			"foo/bar.go",
+			"foo/foo.go",
+		},
+		copyFiles: func(t *testing.T, dest, src string) {
+			destDir := filepath.Join(dest, "foo")
+			srcDir := filepath.Join(src, "foo")
+
+			if err := copyDir(destDir, srcDir); err != nil {
+				t.Fatal(err)
+			}
+		},
+	}
+	test.run(t)
+}
+
+func TestWalkImports(t *testing.T) {
+	dir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	files := []testfile{
+		{
+			"a/p.go",
+			`package a
+
+			import "b"
+			import "c/cc"
+			`,
+		},
+		{
+			"b/p.go",
+			`package b
+			`,
+		},
+		{
+			"c/p.go", // Should ignore this package, nothing imports it.
+			`package c
+			`,
+		},
+		{
+			"c/cc/p.go",
+			`package cc
+			`,
+		},
+	}
+	var (
+		want = []string{"a", "b", "c/cc"}
+		got  []string
+	)
+
+	if err := writeTestFiles(dir, files); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := walkImports(
+		"a",
+		func(pkgName string) string {
+			return filepath.Join(dir, filepath.FromSlash(pkgName))
+		},
+		func(pkg string) (bool, error) {
+			got = append(got, pkg)
+			return true, nil
+		},
+	); err != nil {
+		t.Fatal(err)
+	}
+
+	sort.Strings(got)
+
+	if reflect.DeepEqual(got, want); err != nil {
+		t.Errorf("expected visiting packages %q got %q", want, got)
+	}
+}
+
 type copyTest struct {
-	files []fakefile
+	files []testfile
 	want  []string
 
 	copyFiles func(t *testing.T, dest, src string)
@@ -95,10 +181,8 @@ func (c copyTest) run(t *testing.T) {
 	}
 	defer os.RemoveAll(dest)
 
-	for _, file := range c.files {
-		if err := file.write(src); err != nil {
-			t.Fatal(err)
-		}
+	if err := writeTestFiles(src, c.files); err != nil {
+		t.Fatal(err)
 	}
 
 	c.copyFiles(t, dest, src)
@@ -115,18 +199,27 @@ func (c copyTest) run(t *testing.T) {
 	}
 }
 
-type fakefile struct {
+type testfile struct {
 	path     string
 	contents string
 }
 
-func (f fakefile) write(dir string) error {
+func (f testfile) write(dir string) error {
 	p := filepath.Join(dir, f.path)
 	if err := os.MkdirAll(filepath.Dir(p), 0755); err != nil {
 		return err
 	}
 
 	return ioutil.WriteFile(p, []byte(f.contents), 0644)
+}
+
+func writeTestFiles(dir string, f []testfile) error {
+	for _, file := range f {
+		if err := file.write(dir); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func listFilepaths(dir string) ([]string, error) {
